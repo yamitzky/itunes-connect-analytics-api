3f012f8611f93a7d1c4d09e6fa4ea464cdaf636c
Yamitzky <negiga@gmail.com>
2017-05-11 16:44:12 +0900

    Update to handle error

--- handlers.js
+++ handlers.js
@@ -13,44 +13,48 @@ exports.queryHandler = (instance) => {
     const analyticsQuery = itc.AnalyticsQuery[query.type](query.app, restQuery)[timeKey](...query[timeKey])
 
     instance.request(analyticsQuery, function(error, result) {
-      const data = {}
-      result.results.forEach((groupResult) => {
-        groupResult.data.forEach((row) => {
-          if (dimension) {
-            row[dimension] = groupResult.group.title
+      if (error || !result.results) {
+        res.send(result)
+      } else {
+        const data = {}
+        result.results.forEach((groupResult) => {
+          groupResult.data.forEach((row) => {
+            if (dimension) {
+              row[dimension] = groupResult.group.title
+            }
+            const key = ['date', dimension].map(k => row[k]).join('')
+            data[key] = Object.assign({}, data[key], row)
+          })
+        })
+        const rows = Object.values(data)
+
+        const valuesByColumn = {}
+        rows.forEach((row) => {
+          Object.entries(row).forEach(([ key, value ]) => {
+            valuesByColumn[key] = [ ...(valuesByColumn[key] || []), value ]
+          })
+        })
+        const columns = Object.entries(valuesByColumn).map(([ col, values ]) => {
+          let type = 'string'
+          if (values.every(v => typeof(v) === 'number')) {
+            type = values.every(Number.isInteger) ? 'integer' : 'float'
+          } else if(values.every(v => typeof(v) === 'boolean')) {
+            type = 'boolean'
+          } else if (values.every(v => !Number.isNaN(Date.parse(v)))) {
+            type = values.some(v => v.includes('T')) ? 'datetime' : 'date'
           }
-          const key = ['date', dimension].map(k => row[k]).join('')
-          data[key] = Object.assign({}, data[key], row)
+          return {
+            name: col,
+            friendly_name: col,
+            type: type,
+          }
         })
-      })
-      const rows = Object.values(data)
 
-      const valuesByColumn = {}
-      rows.forEach((row) => {
-        Object.entries(row).forEach(([ key, value ]) => {
-          valuesByColumn[key] = [ ...(valuesByColumn[key] || []), value ]
+        res.send({
+          rows: rows,
+          columns: columns,
         })
-      })
-      const columns = Object.entries(valuesByColumn).map(([ col, values ]) => {
-        let type = 'string'
-        if (values.every(v => typeof(v) === 'number')) {
-          type = values.every(Number.isInteger) ? 'integer' : 'float'
-        } else if(values.every(v => typeof(v) === 'boolean')) {
-          type = 'boolean'
-        } else if (values.every(v => !Number.isNaN(Date.parse(v)))) {
-          type = values.some(v => v.includes('T')) ? 'datetime' : 'date'
-        }
-        return {
-          name: col,
-          friendly_name: col,
-          type: type,
-        }
-      })
-
-      res.send({
-        rows: rows,
-        columns: columns,
-      })
+      }
     })
   }
 }
