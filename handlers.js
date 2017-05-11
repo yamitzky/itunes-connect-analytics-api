544b041af823c711b1822be0859a26ecc9d1db78
Yamitzky <negiga@gmail.com>
2017-05-11 14:46:11 +0900

    Add project files

---
+++ handlers.js
@@ -0,0 +1,56 @@
+const itc = require('itunesconnectanalytics')
+
+
+exports.queryHandler = (instance) => {
+  return (req, res) => {
+    const query = JSON.parse(req.query.q)
+    const dimension = (query.group || {}).dimension
+    const timeKey = query.date ? 'date' : 'time'
+
+    const restQuery = Object.assign({}, query);
+    ['app', 'type', 'date', 'time'].forEach(key => delete restQuery[key])
+
+    const analyticsQuery = itc.AnalyticsQuery[query.type](query.app, restQuery)[timeKey](...query[timeKey])
+
+    instance.request(analyticsQuery, function(error, result) {
+      const data = {}
+      result.results.forEach((groupResult) => {
+        groupResult.data.forEach((row) => {
+          if (dimension) {
+            row[dimension] = groupResult.group.title
+          }
+          const key = ['date', dimension].map(k => row[k]).join('')
+          data[key] = Object.assign({}, data[key], row)
+        })
+      })
+      const rows = Object.values(data)
+
+      const valuesByColumn = {}
+      rows.forEach((row) => {
+        Object.entries(row).forEach(([ key, value ]) => {
+          valuesByColumn[key] = [ ...(valuesByColumn[key] || []), value ]
+        })
+      })
+      const columns = Object.entries(valuesByColumn).map(([ col, values ]) => {
+        let type = 'string'
+        if (values.every(v => typeof(v) === 'number')) {
+          type = values.every(Number.isInteger) ? 'integer' : 'float'
+        } else if(values.every(v => typeof(v) === 'boolean')) {
+          type = 'boolean'
+        } else if (values.every(v => !Number.isNaN(Date.parse(v)))) {
+          type = values.some(v => v.includes('T')) ? 'datetime' : 'date'
+        }
+        return {
+          name: col,
+          friendly_name: col,
+          type: type,
+        }
+      })
+
+      res.send({
+        rows: rows,
+        columns: columns,
+      })
+    })
+  }
+}
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const itc = require('itunesconnectanalytics')
+const program = require('commander')
+
+const handlers = require('./handlers')
+
+program.option('--appleId [appleId]', 'Apple ID to login')
+program.option('--password [password]', 'password to login')
+program.option('--host [host]', 'host to listen')
+program.option('--port [port]', 'port to listen')
+
+const appleId = program.appleId || process.env.APPLE_ID
+const password = program.password || process.env.PASSWORD
+const host = program.host || process.env.HOST || '0.0.0.0'
+const port = program.port || process.env.PORT || 3000
+
+const instance = new itc.Itunes(appleId, password,
+  {
+    errorCallback: (e) => {
+      console.log('Error logging in: ' + e);
+    },
+    successCallback: (d) => {
+      console.log('Logged in');
+    }
+  })
+
+const app = express()
+app.get('/query', handlers.queryHandler(instance))
+app.listen(port, host, () => {
+  console.log('Standby...')
+})
